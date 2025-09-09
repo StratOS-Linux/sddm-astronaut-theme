dc1ec76d7ac2b59bd7a5ddb8bbed034547e3f1cf
ZeStig <69384921+zstg@users.noreply.github.com>
2025-09-09 16:08:57 +0530

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "SDDM Astronaut theme (StratOS version) as a flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    stratos-fonts.url = "github:stratos-linux/stratos-fonts";
+  };
+
+  outputs = { self, nixpkgs, stratos-fonts }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "sddm-astronaut-theme";
+        version = "unstable";
+
+        src = ./.;
+
+        installPhase = ''
+          mkdir -p $out/share/sddm/themes
+          cp -r usr/share/sddm/themes/sddm-astronaut-theme \
+                $out/share/sddm/themes/
+        '';
+
+        meta = with pkgs.lib; {
+          description = "Custom SDDM Astronaut theme (StratOS fork)";
+          license = licenses.gpl3;
+          maintainers = [ "zstg" ];
+          platforms = platforms.linux;
+        };
+      };
+
+      nixosModules.sddm-theme = { config, pkgs, ... }: {
+        config = {
+          services.displayManager.sddm = {
+            enable = true;
+            theme = "sddm-astronaut-theme";
+          };
+
+          # Install theme + fonts globally
+          environment.systemPackages = [
+            self.packages.${system}.default
+            stratos-fonts.packages.${system}.default
+            pkgs.kdePackages.qtmultimedia
+          ];
+
+          # Ensure global fonts are available to Fontconfig
+          fonts.fontDir.enable = true;
+        };
+      };
+    };
+}
