ebf1ca08ad0d23569abd2f11b2b354e8fe744d1d
zstg <o0vckutt@duck.com>
2025-12-15 17:32:13 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,49 +7,54 @@
   };
 
   outputs = { self, nixpkgs, stratos-fonts }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        pname = "sddm-astronaut-theme";
-        version = "unstable";
+    {
+      packages.x86_64-linux.default =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+          pkgs.stdenv.mkDerivation {
+            pname = "sddm-astronaut-theme";
+            version = "unstable";
 
-        src = ./.;
+            src = ./.;
 
-        installPhase = ''
+            installPhase = ''
           mkdir -p $out/share/sddm/themes
           cp -r usr/share/sddm/themes/sddm-astronaut-theme \
                 $out/share/sddm/themes/
         '';
 
-        meta = with pkgs.lib; {
-          description = "Custom SDDM Astronaut theme (StratOS fork)";
-          license = licenses.gpl3;
-          maintainers = [ "zstg" ];
-          platforms = platforms.linux;
-        };
-      };
-
-      nixosModules.sddm-theme = { config, pkgs, ... }: {
-        config = {
-          services.displayManager.sddm = {
-            enable = true;
-            wayland.enable = true;
-            theme = "sddm-astronaut-theme";
+            meta = with pkgs.lib; {
+              description = "Custom SDDM Astronaut theme (StratOS fork)";
+              license = licenses.gpl3;
+              maintainers = [ "zstg" ];
+              platforms = platforms.linux;
+            };
           };
 
-          # Install theme + fonts globally
-          fonts.packages = [ stratos-fonts.packages.${system}.default ];
+      nixosModules.default = { config, lib, pkgs, ... }: {
+        imports = [
+          stratos-fonts.nixosModules.default
+        ];
+
+        config = {
+          fonts.fontconfig.enable = true;
+
+          services.displayManager.sddm = {
+            enable = lib.mkDefault true;
+            wayland.enable = lib.mkDefault false;
+
+            theme = "sddm-astronaut-theme";
+            themePackages = [
+              self.packages.${pkgs.system}.default
+            ];
+          };
+
           environment.systemPackages = [
-            self.packages.${system}.default
-            stratos-fonts.packages.${system}.default # this probably isn't necessary
             pkgs.kdePackages.qtmultimedia
           ];
-
-          # Ensure global fonts are available to Fontconfig
-          fonts.fontDir.enable = true;
         };
       };
+
     };
 }
